ab7d21e60f2858ef750b7c50a7dce7f119def323
Soporte3w <167887286+Soporte3w@users.noreply.github.com>
2024-11-25 15:47:42 -0500

    Se quito ruta redundante de .gitignore

--- Client/src/pages/CampanasCall.js
+++ Client/src/pages/CampanasCall.js
@@ -164,7 +164,7 @@ function Campanas() {
         title: "Error",
         text: "Todos los campos deben estar completos y debes adjuntar un archivo válidos.",
         customClass: {
-          popup: "my-swal-popup", // Añade una clase personalizada al modal
+          popup: "my-swal-popup",
         },
         background: "#111111",
       });
